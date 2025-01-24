4e59b121e2616b177d6a696e8b7f8db2a84fda0a
Enzo Cortez <enzo.cortez@udla.edu.ec>
2025-01-24 12:24:09 -0500

    Agregar archivos de proyecto.

---
+++ ApiMomentum/Models/TaskItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiMomentum.Models
+{
+    public class TaskItem
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public string Category { get; set; }
+        public int Priority { get; set; }
+    }
+}
+
+
